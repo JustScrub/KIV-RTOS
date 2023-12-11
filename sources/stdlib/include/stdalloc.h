180fb602654dab2a3b34548c1f0ab3d310ed931c
Jakub Mladý <scrub@gapps.zcu.cz>
2023-12-11 11:26:45 +0100

    malloc: page number calculation correction

--- sources/stdlib/include/stdalloc.h
+++ sources/stdlib/include/stdalloc.h
@@ -6,7 +6,5 @@
 #include <stdfile.h>
 
 uint32_t __stdalloc_mem_offset = 0;
-uint32_t __stdalloc_first_alloc = 0;
-
 
 void *malloc(size_t size);
--- sources/stdlib/src/stdalloc.cpp
+++ sources/stdlib/src/stdalloc.cpp
@@ -1,5 +1,10 @@
 #include <stdalloc.h>
 
+inline int32_t sz2pgs(uint32_t size)
+{
+    return size / mem::PageSize + (bool)(size % mem::PageSize);
+}
+
 void *malloc(size_t size)
 {
     uint32_t addr = Task_Heap_Start + __stdalloc_mem_offset;
@@ -7,12 +12,10 @@ void *malloc(size_t size)
     if(addr + size > Task_Heap_End || size == 0)
         return nullptr;
 
-    uint32_t pages = (__stdalloc_mem_offset+size) / mem::PageSize - __stdalloc_mem_offset / mem::PageSize;
-    pages += __stdalloc_first_alloc; // at first alloc, we need to allocate one more page for heap start
+    int32_t pages = sz2pgs(__stdalloc_mem_offset + size) - sz2pgs(__stdalloc_mem_offset);
 
-    if(i_am_once_again_asking_for_your_memory_support(pages) == nullptr)
+    if(pages > 0 && i_am_once_again_asking_for_your_memory_support(pages) == nullptr)
         return nullptr;
-    __stdalloc_first_alloc = 0;
     __stdalloc_mem_offset += size;
     return (void *)addr;
 }
