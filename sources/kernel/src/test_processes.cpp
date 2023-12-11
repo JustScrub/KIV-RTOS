cb4bfe180f112626d525896b73d8d1702d9bf877
Jakub Mladý <scrub@gapps.zcu.cz>
2023-12-11 12:22:25 +0100

    Malloc test task

--- sources/kernel/src/test_processes.cpp
+++ sources/kernel/src/test_processes.cpp
@@ -1,8 +1,9 @@
 #include "../../userspace/build/src_init_task.h"
-#include "../../userspace/build/src_sos_task.h"
-#include "../../userspace/build/src_oled_task.h"
-#include "../../userspace/build/src_logger_task.h"
-#include "../../userspace/build/src_counter_task.h"
-#include "../../userspace/build/src_tilt_task.h"
+//#include "../../userspace/build/src_sos_task.h"
+//#include "../../userspace/build/src_oled_task.h"
+//#include "../../userspace/build/src_logger_task.h"
+//#include "../../userspace/build/src_counter_task.h"
+//#include "../../userspace/build/src_tilt_task.h"
+#include "../../userspace/build/src_heap_test_task.h"
 
 // az budeme umet cist SD kartu, tento soubor uplne zmizi
